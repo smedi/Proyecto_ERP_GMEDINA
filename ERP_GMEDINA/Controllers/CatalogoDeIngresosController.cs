58f5b7b1bb43699c1970de2887c1c3d0c56b7d64
fnhrndzi <57505350+fnhrndzi@users.noreply.github.com>
2019-11-14 15:06:35 -0600

    Agregre tbCatalogoDeIngreso
    Class del Modelo, Vista, Controlador, scripts js.

---
+++ ERP_GMEDINA/Controllers/CatalogoDeIngresosController.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ERP_GMEDINA.Models;
+
+namespace ERP_GMEDINA.Controllers
+{
+    public class CatalogoDeIngresosController : Controller
+    {
+        private ERP_GMEDINAEntities db = new ERP_GMEDINAEntities();
+
+
+        // GET: tbCatalogoDeIngresos
+        public ActionResult Index()
+        {
+            //var INNERJOIN = from tbCatIngreso in db.tbCatalogoDeIngresos
+            //                where tbCatIngreso.cin_Activo == true
+            //                join tbUsuCrea in db.tbUsuario on tbCatIngreso.cin_UsuarioCrea equals tbUsuCrea.usu_Id
+            //                select new { tbCatIngreso.cin_IdIngreso,
+            //                             tbCatIngreso.cin_DescripcionIngreso,
+            //                             tbCatIngreso.cin_Activo,
+            //                             tbCatIngreso.cin_UsuarioCrea,
+            //                             tbCatIngreso.cin_FechaCrea,
+            //                             tbCatIngreso.cin_UsuarioModifica,
+            //                             tbCatIngreso.cin_FechaModifica
+            //                           };
+
+            //var tbCatalogoDeIngresos1 = db.tbCatalogoDeIngresos
+             //           .Select(c => new { cin_DescripcionIngreso = c.cin_DescripcionIngreso, cin_IdIngreso = c.cin_IdIngreso, cin_UsuarioCrea = c.cin_UsuarioCrea, usu_NombreUsuario = c.tbUsuario.usu_NombreUsuario, cin_FechaCrea = c.cin_FechaCrea, cin_UsuarioModifica = c.cin_UsuarioModifica, cin_FechaModifica= c.cin_FechaModifica, cin_Activo= c.cin_Activo}).Where(c => c.cin_Activo == true)
+             //           .ToList();
+            var tbCatalogoDeIngresos = db.tbCatalogoDeIngresos.Include(t => t.tbUsuario).Include(t => t.tbUsuario1).Where(x => x.cin_Activo == true);
+            return View(tbCatalogoDeIngresos.ToList());
+        }
+
+        public ActionResult GetData()
+        {
+            //SI SE LLEGA A DAR PROBLEMAS DE "REFERENCIAS CIRCULARES", OBTENER LA DATA DE ESTA FORMA
+            //SELECCIONANDO UNO POR UNO LOS CAMPOS QUE NECESITAREMOS
+            //DE LO CONTRARIO, HACERLO DE LA FORMA CONVENCIONAL (EJEMPLO: db.tbCatalogoDeDeducciones.ToList(); )
+            var tbCatalogoDeIngresos1 = db.tbCatalogoDeIngresos               
+                        .Select(c => new {
+                                           cin_IdIngresos = c.cin_IdIngreso,
+                                           cin_DescripcionIngreso = c.cin_DescripcionIngreso,
+                                           cin_Activo = c.cin_Activo,
+                                           cin_UsuarioCrea = c.cin_UsuarioCrea,
+                                           cin_FechaCrea = c.cin_FechaCrea,
+                                           cin_UsuarioModifica= c.cin_UsuarioModifica,
+                                           cin_FechaModifica = c.cin_FechaModifica})
+                                           .Where(x => x.cin_Activo == true)
+                        .ToList();
+            //RETORNAR JSON AL LADO DEL CLIENTE
+            return new JsonResult { Data = tbCatalogoDeIngresos1, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "cin_IdIngreso,cin_DescripcionIngreso,cin_UsuarioCrea,cin_FechaCrea,cin_UsuarioModifica,cin_FechaModifica,cin_Activo")] tbCatalogoDeIngresos tbCatalogoDeIngresos)
+        {
+
+            //LLENAR LA DATA DE AUDITORIA, DE NO HACERLO EL MODELO NO SERÍA VÁLIDO Y SIEMPRE CAERÍA EN EL CATCH
+            tbCatalogoDeIngresos.cin_UsuarioCrea = 1;
+            tbCatalogoDeIngresos.cin_FechaCrea = DateTime.Now;
+            //VARIABLE PARA ALMACENAR EL RESULTADO DEL PROCESO Y ENVIARLO AL LADO DEL CLIENTE
+            string response = String.Empty;
+            IEnumerable<object> listCatalogoDeIngresos = null;
+            string MensajeError = "";
+            //VALIDAR SI EL MODELO ES VÁLIDO
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //EJECUTAR PROCEDIMIENTO ALMACENADO
+                    listCatalogoDeIngresos = db.UDP_Plani_tbCatalogoDeIngresos_Insert(tbCatalogoDeIngresos.cin_DescripcionIngreso,
+                                                                                         tbCatalogoDeIngresos.cin_UsuarioCrea,
+                                                                                         tbCatalogoDeIngresos.cin_FechaCrea);
+                    //RECORRER EL TIPO COMPLEJO DEL PROCEDIMIENTO ALMACENADO PARA EVALUAR EL RESULTADO DEL SP
+                    foreach (UDP_Plani_tbCatalogoDeDeducciones_Insert_Result Resultado in listCatalogoDeIngresos)
+                        MensajeError = Resultado.MensajeError;
+
+                    if (MensajeError.StartsWith("-1"))
+                    {
+                        //EN CASO DE OCURRIR UN ERROR, IGUALAMOS LA VARIABLE "RESPONSE" A ERROR PARA VALIDARLO EN EL CLIENTE
+                        ModelState.AddModelError("", "No se pudo ingresar el registro, contacte al administrador");
+                        response = "error";
+                    }
+
+                }
+                catch (Exception Ex)
+                {
+                    //EN CASO DE CAER EN EL CATCH, IGUALAMOS LA VARIABLE "RESPONSE" A ERROR PARA VALIDARLO EN EL CLIENTE
+                    response = Ex.Message.ToString();
+                }
+                //SI LA EJECUCIÓN LLEGA A ESTE PUNTO SIGNIFICA QUE NO OCURRIÓ NINGÚN ERROR Y EL PROCESO FUE EXITOSO
+                //IGUALAMOS LA VARIABLE "RESPONSE" A "BIEN" PARA VALIDARLO EN EL CLIENTE
+                response = "bien";
+            }
+            else
+            {
+                //SI EL MODELO NO ES VÁLIDO, IGUALAMOS LA VARIABLE "RESPONSE" A ERROR PARA VALIDARLO EN EL CLIENTE
+                response = "error";
+            }
+
+            ViewBag.cin_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbCatalogoDeIngresos.cin_UsuarioCrea);
+            ViewBag.cin_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbCatalogoDeIngresos.cin_UsuarioModifica);
+            return View(tbCatalogoDeIngresos);
+        }
+
+        // GET: CatalogoDeIngresos/Edit/5
+        public JsonResult Edit(int? ID)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            tbCatalogoDeIngresos tbCatalogoDeIngresosJSON = db.tbCatalogoDeIngresos.Find(ID);
+            return Json(tbCatalogoDeIngresosJSON, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Details(int? ID)
+        {
+            var tbCatalogoDeIngresosJSON = from tbCatIngreso in db.tbCatalogoDeIngresos
+                                           //join tbUsuCrea in db.tbUsuario on tbCatIngreso.cin_UsuarioCrea equals tbUsuCrea.usu_Id
+                                           //join tbUsuModi in db.tbUsuario on tbCatIngreso.cin_UsuarioModifica equals tbUsuModi.usu_Id
+                                           where tbCatIngreso.cin_Activo == true && tbCatIngreso.cin_IdIngreso == ID
+                                           select new
+                                           {
+                                               tbCatIngreso.cin_IdIngreso,
+                                               tbCatIngreso.cin_DescripcionIngreso,
+                                               tbCatIngreso.cin_Activo,
+                                               tbCatIngreso.cin_UsuarioCrea,
+                                               UsuCrea= tbCatIngreso.tbUsuario.usu_NombreUsuario,
+                                               tbCatIngreso.cin_FechaCrea,
+                                               tbCatIngreso.cin_UsuarioModifica,
+                                               UsuModifica= tbCatIngreso.tbUsuario1.usu_NombreUsuario,
+                                               tbCatIngreso.cin_FechaModifica
+                                           };
+
+
+            db.Configuration.ProxyCreationEnabled = false;
+            //tbCatalogoDeIngresos tbCatalogoDeIngresosJSON = db.tbCatalogoDeIngresos.Include(t => t.tbUsuario).Include(t => t.tbUsuario1).Find(ID);
+            return Json(tbCatalogoDeIngresosJSON, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "cin_IdIngreso,cin_DescripcionIngreso,cin_UsuarioModifica,cin_FechaModifica,cin_Activo")] tbCatalogoDeIngresos tbCatalogoDeIngresos)
+        {
+            //DATA DE AUDIOTIRIA DE CREACIÓN, PUESTA UNICAMENTE PARA QUE NO CAIGA EN EL CATCH
+            //EN EL PROCEDIMIENTO ALMACENADO, ESTOS DOS CAMPOS NO SE DEBEN MODIFICAR
+            //tbCatalogoDeIngresos.cin_UsuarioCrea = 1;
+            //tbCatalogoDeIngresos.cin_FechaCrea = DateTime.Now;
+
+
+            //LLENAR DATA DE AUDITORIA
+            tbCatalogoDeIngresos.cin_UsuarioModifica = 1;
+            tbCatalogoDeIngresos.cin_FechaModifica = DateTime.Now;
+            string response = String.Empty;
+            IEnumerable<object> listCatalogoDeIngresos = null;
+            string MensajeError = "";
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //EJECUTAR PROCEDIMIENTO ALMACENADO
+                    listCatalogoDeIngresos = db.UDP_Plani_tbCatalogoDeIngresos_Update(tbCatalogoDeIngresos.cin_IdIngreso,
+                                                                                            tbCatalogoDeIngresos.cin_DescripcionIngreso,
+                                                                                            tbCatalogoDeIngresos.cin_UsuarioModifica,
+                                                                                            tbCatalogoDeIngresos.cin_FechaModifica
+                                                                                            );
+                    //RECORRER EL TIPO COMPLEJO DEL PROCEDIMIENTO ALMACENADO PARA EVALUAR EL RESULTADO DEL SP
+                    foreach (UDP_Plani_tbCatalogoDeIngresos_Update_Result Resultado in listCatalogoDeIngresos)
+                        MensajeError = Resultado.MensajeError;
+
+
+
+                    if (MensajeError.StartsWith("-1"))
+                    {
+                        //EN CASO DE OCURRIR UN ERROR, IGUALAMOS LA VARIABLE "RESPONSE" A ERROR PARA VALIDARLO EN EL CLIENTE
+                        ModelState.AddModelError("", "No se pudo ingresar el registro, contacte al administrador");
+                        response = "error";
+                    }
+                }
+                catch (Exception Ex)
+                {
+                    //EN CASO DE CAER EN EL CATCH, IGUALAMOS LA VARIABLE "RESPONSE" A ERROR PARA VALIDARLO EN EL CLIENTE
+                    ModelState.AddModelError("", "No se pudo modificar el registro, contacte al administrador.");
+                    response = "error";
+                }
+            }
+            else
+            {
+                // SI EL MODELO NO ES CORRECTO, RETORNAR ERROR
+                ModelState.AddModelError("", "No se pudo modificar el registro, contacte al administrador.");
+                response = "error";
+            }
+          
+
+
+            //RETORNAR MENSAJE AL LADO DEL CLIENTE
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+       
+
+        public JsonResult Inactivar(int? ID)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            tbCatalogoDeIngresos tbCatalogoDeIngresosJSON = db.tbCatalogoDeIngresos.Find(ID);
+            return Json(tbCatalogoDeIngresosJSON, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Inactivar([Bind(Include = "cin_IdIngreso,cin_UsuarioModifica,cin_FechaModifica")] tbCatalogoDeIngresos tbCatalogoDeIngresos)
+        {
+            //DATA DE AUDIOTIRIA DE CREACIÓN, PUESTA UNICAMENTE PARA QUE NO CAIGA EN EL CATCH
+            //EN EL PROCEDIMIENTO ALMACENADO, ESTOS DOS CAMPOS NO SE DEBEN MODIFICAR
+            //tbCatalogoDeIngresos.cin_UsuarioCrea = 1;
+            //tbCatalogoDeIngresos.cin_FechaCrea = DateTime.Now;
+
+
+            //LLENAR DATA DE AUDITORIA
+            tbCatalogoDeIngresos.cin_UsuarioModifica = 1;
+            tbCatalogoDeIngresos.cin_FechaModifica = DateTime.Now;
+            string response = String.Empty;
+            IEnumerable<object> listCatalogoDeIngresos = null;
+            string MensajeError = "";
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //EJECUTAR PROCEDIMIENTO ALMACENADO
+                    listCatalogoDeIngresos = db.UDP_Plani_tbCatalogoDeIngresos_Inactivar(tbCatalogoDeIngresos.cin_IdIngreso,
+                                                                                            tbCatalogoDeIngresos.cin_UsuarioModifica,
+                                                                                            tbCatalogoDeIngresos.cin_FechaModifica
+                                                                                            );
+                    //RECORRER EL TIPO COMPLEJO DEL PROCEDIMIENTO ALMACENADO PARA EVALUAR EL RESULTADO DEL SP
+                    foreach (UDP_Plani_tbCatalogoDeIngresos_Inactivar_Result Resultado in listCatalogoDeIngresos)
+                        MensajeError = Resultado.MensajeError;
+
+
+                    //RETORNAR MENSAJE DE CONFIRMACIÓN EN CASO QUE NO HAYA CAIDO EN EL CATCH
+                    response = "bien";
+                }
+                catch (Exception e)
+                {
+                    //EN CASO DE CAER EN EL CATCH, IGUALAMOS LA VARIABLE "RESPONSE" A ERROR PARA VALIDARLO EN EL CLIENTE
+                    ModelState.AddModelError("", "No se pudo modificar el registro, contacte al administrador.");
+                    response = "error";
+                }
+            }
+            else
+            {
+                // SI EL MODELO NO ES CORRECTO, RETORNAR ERROR
+                ModelState.AddModelError("", "No se pudo modificar el registro, contacte al administrador.");
+                response = "error";
+            }
+            //RETORNAR MENSAJE AL LADO DEL CLIENTE
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+        //FUNCIÓN: OBETENER LA DATA PARA LLENAR LOS DROPDOWNLIST DE EDICIÓN Y CREACIÓN
+        //public JsonResult EditGetDDL()
+        //{
+        //    //OBTENER LA DATA QUE NECESITAMOS, HACIENDOLO DE ESTA FORMA SE EVITA LA EXCEPCION POR "REFERENCIAS CIRCULARES"
+        //    var DDL =
+        //    from TipoDedu in db.tbTipoDeduccion
+        //    join CatDeduc in db.tbCatalogoDeDeducciones on TipoDedu.tde_IdTipoDedu equals CatDeduc.tde_IdTipoDedu into prodGroup
+        //    select new { Id = TipoDedu.tde_IdTipoDedu, Descripcion = TipoDedu.tde_Descripcion };
+        //    //RETORNAR LA DATA EN FORMATO JSON AL CLIENTE 
+        //    return Json(DDL, JsonRequestBehavior.AllowGet);
+        //}
+
+        // GET: CatalogoDeDeducciones/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    tbCatalogoDeIngresos tbCatalogoDeIngresos = db.tbCatalogoDeIngresos.Find(id);
+        //    if (tbCatalogoDeIngresos == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(tbCatalogoDeIngresos);
+        //}
+
+        // GET: CatalogoDeDeducciones/Create
+
+        // GET: CatalogoDeDeducciones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbCatalogoDeIngresos tbCatalogoDeIngresos = db.tbCatalogoDeIngresos.Find(id);
+            if (tbCatalogoDeIngresos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbCatalogoDeIngresos);
+        }
+
+        // POST: CatalogoDeDeducciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbCatalogoDeIngresos tbCatalogoDeIngresos = db.tbCatalogoDeIngresos.Find(id);
+            db.tbCatalogoDeIngresos.Remove(tbCatalogoDeIngresos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ERP_GMEDINA/Scripts/app/General/CatalogoDeIngresos.js
@@ -0,0 +1,249 @@
+﻿//
+//OBTENER SCRIPT DE FORMATEO DE FECHA
+//
+$.getScript("../Scripts/app/General/SerializeDate.js")
+  .done(function (script, textStatus) {
+      console.log(textStatus);
+  })
+  .fail(function (jqxhr, settings, exception) {
+      console.log("No se pudo recuperar Script SerializeDate");
+  });
+
+//FUNCION GENERICA PARA REUTILIZAR AJAX
+function _ajax(params, uri, type, callback) {
+    $.ajax({
+        url: uri,
+        type: type,
+        data: { params },
+        success: function (data) {
+            callback(data);
+        }
+    });
+}
+
+function cargarGridIngresos() {
+    _ajax(null,
+        '/CatalogoDeIngresos/GetData',
+        'GET',
+        (data) => {
+            if (data.length == 0) {
+                //Validar si se genera un error al cargar de nuevo el grid
+                iziToast.error({
+                    title: 'Error',
+                    message: 'No se pudo cargar la información, contacte al administrador',
+                });
+            }
+            //GUARDAR EN UNA VARIABLE LA DATA OBTENIDA
+            var ListaIngresos = data, template = '';
+            //RECORRER DATA OBETINA Y CREAR UN "TEMPLATE" PARA REFRESCAR EL TBODY DE LA TABLA DEL INDEX
+            for (var i = 0; i < ListaIngresos.length; i++) {
+                template += '<tr data-id = "' + ListaIngresos[i].cin_IdIngresos + '">' +
+                    '<td>' + ListaIngresos[i].cin_IdIngresos + '</td>' +
+                    '<td>' + ListaIngresos[i].cin_DescripcionIngreso + '</td>' +
+                    '<td>' +
+                    '<button type="button" class="btn btn-primary btn-xs" id="btnEditarIngreso">Editar</button>' +
+                    '<button type="button" class="btn btn-default btn-xs" id="btnDetalle">Detalle</button>' +
+                    //'<button type="button" class="btn btn-danger btn-xs" id="btnInactivarIngreso">Inactivar</button>'+
+                    '</td>' +
+                    '</tr>';
+            }
+            //REFRESCAR EL TBODY DE LA TABLA DEL INDEX
+            $('#tbodyIngresos').html(template);
+        });
+}
+
+
+//DETALLES
+$(document).on("click", "#tblCatalogoIngresos tbody tr td #btnDetalle", function () {
+    var ID = $(this).closest('tr').data('id');
+    $.ajax({
+        url: "/CatalogoDeIngresos/Details/" + ID,
+        method: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({ ID: ID })
+    })
+        .done(function (data) {$("#DetailCatalogoIngresos").modal();
+            //SI SE OBTIENE DATA, LLENAR LOS CAMPOS DEL MODAL CON ELLA
+        if (data) {
+            console.log(data);
+                var FechaCrea = FechaFormato(data[0].cin_FechaCrea);
+                var FechaModifica = FechaFormato(data[0].cin_FechaModifica);
+                $("#Detallar #cin_IdIngreso").val(data[0].cin_IdIngreso);
+                $("#Detallar #cin_DescripcionIngreso").val(data[0].cin_DescripcionIngreso);
+                $("#Detallar #cin_UsuarioCrea").val(data[0].cin_UsuarioCrea);
+                $("#Detallar #tbUsuario_usu_NombreUsuario").val(data[0].UsuCrea);
+                $("#Detallar #cin_FechaCrea").val(FechaCrea);
+                data[0].UsuModifica == null ? $("#Detallar #tbUsuario1_usu_NombreUsuario").val('Sin modificaciones'): $("#Detallar #tbUsuario1_usu_NombreUsuario").val(data[0].UsuModifica);
+                $("#Detallar #cin_UsuarioModifica").val(data[0].cin_UsuarioModifica);
+                $("#Detallar #cin_FechaModifica").val(FechaModifica);
+                
+            }
+            else {
+                //Mensaje de error si no hay data
+                iziToast.error({
+                    title: 'Error',
+                    message: 'No se pudo cargar la información, contacte al administrador',
+                });
+            }
+        });
+});
+
+//FUNCION: PRIMERA FASE DE EDICION DE REGISTROS, MOSTRAR MODAL CON LA INFORMACIÓN DEL REGISTRO SELECCIONADO
+$(document).on("click", "#tblCatalogoIngresos tbody tr td #btnEditarIngreso", function () {
+    var ID = $(this).closest('tr').data('id');
+    $.ajax({
+        url: "/CatalogoDeIngresos/Edit/" + ID,
+        method: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({ ID: ID })
+    })
+        .done(function (data) {
+            //SI SE OBTIENE DATA, LLENAR LOS CAMPOS DEL MODAL CON ELLA
+            if (data) {
+                $("#Editar #cin_IdIngreso").val(data.cin_IdIngreso);
+                $("#Editar #cin_DescripcionIngreso").val(data.cin_DescripcionIngreso);
+                $("#EditarCatalogoIngresos").modal();
+            }
+            else {
+                //Mensaje de error si no hay data
+                iziToast.error({
+                    title: 'Error',
+                    message: 'No se pudo cargar la información, contacte al administrador',
+                });
+            }
+        });
+});
+
+
+
+
+
+$("#btnModalInactivar").click(function () {
+    $("#InactivarCatalogoIngresos").modal();
+});
+
+
+
+$("#btnInactivarIngresos").click(function () {
+    //SERIALIZAR EL FORMULARIO (QUE ESTÁ EN LA VISTA PARCIAL) DEL MODAL, SE PARSEA A FORMATO JSON
+    var data = $("#frmInactivarCatalogoIngresos").serializeArray();
+    //SE ENVIA EL JSON AL SERVIDOR PARA EJECUTAR LA EDICIÓN
+    $.ajax({
+        url: "/CatalogoDeIngresos/Inactivar",
+        method: "POST",
+        data: data
+    }).done(function (data) {
+        if (data == "error") {
+            //Cuando traiga un error del backend al guardar la edicion
+            iziToast.error({
+                title: 'Error',
+                message: 'No se pudo inactivar el registro, contacte al administrador',
+            });
+        }
+        else {
+            $("#InactivarCatalogoIngresos").modal('hide');
+            $("#EditarCatalogoIngresos").modal('hide');
+            cargarGridIngresos();
+            //Mensaje de exito de la edicion
+            iziToast.success({
+                title: 'Exito',
+                message: 'El registro fue inactivado de forma exitosa!',
+            });
+        }
+    });
+});
+
+
+//EJECUTAR EDICIÓN DEL REGISTRO EN EL MODAL
+$("#btnUpdateIngresos").click(function () {
+    //SERIALIZAR EL FORMULARIO (QUE ESTÁ EN LA VISTA PARCIAL) DEL MODAL, SE PARSEA A FORMATO JSON
+    var data = $("#frmCatalogoIngresos").serializeArray();
+    //SE ENVIA EL JSON AL SERVIDOR PARA EJECUTAR LA EDICIÓN
+    $.ajax({
+        url: "/CatalogoDeIngresos/Edit",
+        method: "POST",
+        data: data
+    }).done(function (data) {
+        if (data == "error") {
+            //Cuando traiga un error del backend al guardar la edicion
+            iziToast.error({
+                title: 'Error',
+                message: 'No se pudo editar el registro, contacte al administrador',
+            });
+        }
+        else {
+            // REFRESCAR UNICAMENTE LA TABLA
+            
+            //UNA VEZ REFRESCADA LA TABLA, SE OCULTA EL MODAL
+            $("#EditarCatalogoIngresos").modal('hide');
+            cargarGridIngresos();
+            //Mensaje de exito de la edicion
+            iziToast.success({
+                title: 'Exito',
+                message: 'El registro fue editado de forma exitosa!',
+            });
+        }
+    });
+});
+
+//FUNCION: PRIMERA FASE DE AGREGAR UN NUEVO REGISTRO, MOSTRAR MODAL DE CREATE
+$(document).on("click", "#btnAgregarCatalogoIngresos", function () {
+    //MOSTRAR EL MODAL DE AGREGAR
+    $("#AgregarCatalogoIngresos").modal();
+});
+
+//FUNCION: CREAR EL NUEVO REGISTRO
+$('#btnCreateRegistroIngresos').click(function () {
+    // SIEMPRE HACER LAS RESPECTIVAS VALIDACIONES DEL LADO DEL CLIENTE
+
+    //SERIALIZAR EL FORMULARIO DEL MODAL (ESTÁ EN LA VISTA PARCIAL)
+    var data = $("#frmCatalogoIngresosCreate").serializeArray();
+    //ENVIAR DATA AL SERVIDOR PARA EJECUTAR LA INSERCIÓN
+    $.ajax({
+        url: "/CatalogoDeIngresos/Create",
+        method: "POST",
+        data: data
+    }).done(function (data) {
+        //CERRAR EL MODAL DE AGREGAR
+       
+        //VALIDAR RESPUESTA OBETNIDA DEL SERVIDOR, SI LA INSERCIÓN FUE EXITOSA O HUBO ALGÚN ERROR
+        debugger;
+         //data = ' ';
+        if ("#cin_DescripcionIngreso" == '') {
+           // $("#Crear #cin_DescripcionIngreso").focus;
+            $("#Crear #cin_DescripcionIngreso").focus;
+            iziToast.error({
+                title: 'Error',
+                message: 'No se pudo guardar el registros vacios',
+            });
+        }
+        else {
+            if (data == "error") {
+                $("#AgregarCatalogoIngresos").modal('hide');
+                iziToast.error({
+                    title: 'Error',
+                    message: 'No se pudo guardar el registro, contacte al administrador',
+                });
+            }
+            else {
+                $("#AgregarCatalogoIngresos").modal('hide');
+                cargarGridIngresos();
+                $("#Crear #cin_DescripcionIngreso").val('');
+                // Mensaje de exito cuando un registro se ha guardado bien
+                iziToast.success({
+                    title: 'Exito',
+                    message: 'El registro fue registrado de forma exitosa!',
+                });
+            }
+        }
+    });
+});
+
+
+//FUNCION: OCULTAR MODAL DE EDICIÓN
+$("#btnCerrarEditar").click(function () {
+    $("#EditarCatalogoIngresos").modal('hide');
+});
+
