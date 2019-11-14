a0b261d12df9fdb8fe105bd5f8d7cb9d6bc2559a
Lainez19 <abi.lainez00@gmail.com>
2019-11-14 15:12:30 -0600

    Creación de vistas y controlador de la tabla Tipo Deducción

---
+++ ERP_GMEDINA/Controllers/TipoDeduccionesController.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ERP_GMEDINA.Models;
+
+namespace ERP_GMEDINA.Controllers
+{
+    public class TipoDeduccionesController : Controller
+    {
+        private ERP_GMEDINAEntities db = new ERP_GMEDINAEntities();
+
+        // GET: TipoDeducciones
+        public ActionResult Index()
+        {
+            var tbTipoDeduccion = db.tbTipoDeduccion.Where(x => x.tde_Activo==true).Include(t => t.tbUsuario).Include(t => t.tbUsuario1);
+            return View(tbTipoDeduccion.ToList());
+        }
+
+        public ActionResult GetData()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            var tbTipoDeducciones = db.tbTipoDeduccion.ToList();
+            return new JsonResult { Data = tbTipoDeducciones, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        // GET: TipoDeducciones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbTipoDeduccion tbTipoDeduccion = db.tbTipoDeduccion.Find(id);
+            if (tbTipoDeduccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbTipoDeduccion);
+        }
+
+        // POST: TipoDeduccion/Create REALIZAR LA INSERCIÓN
+        // GET: TipoDeducciones/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "tde_IdTipoDedu, tde_Descripcion, tde_UsuarioCrea, tde_FechaCrea")] tbTipoDeduccion tbTipoDeduccion)
+        {
+            //LLENAR LA DATA DE AUDITORIA, DE NO HACERLO EL MODELO NO SERÍA VÁLIDO Y SIEMPRE CAERÍA EN EL CATCH
+            tbTipoDeduccion.tde_UsuarioCrea = 1;
+            tbTipoDeduccion.tde_FechaCrea = DateTime.Now;
+            //VARIABLE PARA ALMACENAR EL RESULTADO DEL PROCESO Y ENVIARLO AL LADO DEL CLIENTE
+            string response = String.Empty;
+            IEnumerable<object> listTipoDeduccion = null;
+            String MessageError = "";
+            //VALIDAR SI EL MODELO ES VÁLIDO
+            if (ModelState.IsValid)
+               {
+                try
+                {
+                    //EJECUTAR PROCEDIMIENTO ALMACENADO
+                    listTipoDeduccion = db.UDP_Plani_tbTipoDeduccion_Insert(tbTipoDeduccion.tde_Descripcion,
+                                                                            tbTipoDeduccion.tde_UsuarioCrea,
+                                                                            tbTipoDeduccion.tde_FechaCrea);
+                    //RECORRER EL TIPO COMPLEJO DEL PROCEDIMIENTO ALMACENADO PARA EVALUAR EL RESULTADO DEL SP                                                  
+                    foreach (UDP_Plani_tbTipoDeduccion_Insert_Result resultado in listTipoDeduccion)
+                        MessageError = resultado.MensajeError;
+
+                    if(MessageError.StartsWith("-1"))
+                    {
+                        //EN CASO DE OCURRIR UN ERROR, IGUALAMOS LA VARIABLE "RESPONSE" A ERROR PARA VALIDARLO EN EL CLIENTE
+                        ModelState.AddModelError("", "No se pudo ingresar el registro, contacte al administrador");
+                        response = "error";
+                    }
+                }
+                catch (Exception Ex)
+                {
+                    //EN CASO DE CAER EN EL CATCH, IGUALAMOS LA VARIABLE "RESPONSE" A ERROR PARA VALIDARLO EN EL CLIENTE
+                    ModelState.AddModelError("", "No se pudo ingresar el registro, contacte al administrador");
+                    response = "error" + Ex.Message.ToString();
+                }
+                //SI LA EJECUCIÓN LLEGA A ESTE PUNTO SIGNIFICA QUE NO OCURRIÓ NINGÚN ERROR Y EL PROCESO FUE EXITOSO
+                //IGUALAMOS LA VARIABLE "RESPONSE" A "BIEN" PARA VALIDARLO EN EL CLIENTE
+                response = "bien";
+               }
+            else
+            {
+                //SI EL MODELO NO ES VÁLIDO, IGUALAMOS LA VARIABLE "RESPONSE" A ERROR PARA VALIDARLO EN EL CLIENTE
+                response = "error";
+            }
+            //RETORNAMOS LA VARIABLE RESPONSE AL CLIENTE PARA EVALUARLA
+            ViewBag.tde_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTipoDeduccion.tde_UsuarioCrea);
+            ViewBag.tde_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTipoDeduccion.tde_UsuarioModifica);
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+
+        // POST: TipoDeducciones/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+       
+
+        // GET: TipoDeducciones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var tbTipoDeduccion = db.tbTipoDeduccion.Where(d=> d.tde_IdTipoDedu == id)
+                        .Select(c => new { tde_Descripcion = c.tde_Descripcion, tde_IdTipoDedu = c.tde_IdTipoDedu, tde_UsuarioCrea = c.tde_UsuarioCrea, tde_FechaCrea = c.tde_FechaCrea, tde_UsuarioModifica = c.tde_UsuarioModifica, tde_FechaModifica = c.tde_FechaModifica, tde_Activo = c.tde_Activo })
+                        .ToList();
+
+            //db.Configuration.ProxyCreationEnabled = false;
+            //tbTipoDeduccion tbTipoDeduccion = db.tbTipoDeduccion.Find(id);
+            //RETORNAR JSON AL LADO DEL CLIENTE              
+            if (tbTipoDeduccion == null)
+            {
+                return HttpNotFound();
+            }
+            //ViewBag.tde_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTipoDeduccion.tde_UsuarioCrea);
+            //ViewBag.tde_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTipoDeduccion.tde_UsuarioModifica);
+            return new JsonResult { Data = tbTipoDeduccion, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    tbTipoDeduccion tbTipoDeduccion = db.tbTipoDeduccion.Find(id);
+        //    if (tbTipoDeduccion == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(tbTipoDeduccion);
+        //}
+
+        // POST: TipoDeducciones/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "tde_IdTipoDedu,tde_Descripcion,tde_UsuarioCrea,tde_FechaCrea,tde_UsuarioModifica,tde_FechaModifica,tde_Activo")] tbTipoDeduccion tbTipoDeduccion)
+        {
+            //DATA DE AUDIOTIRIA DE CREACIÓN, PUESTA UNICAMENTE PARA QUE NO CAIGA EN EL CATCH
+            //EN EL PROCEDIMIENTO ALMACENADO, ESTOS DOS CAMPOS NO SE DEBEN MODIFICAR
+            tbTipoDeduccion.tde_UsuarioCrea = 1;
+            tbTipoDeduccion.tde_FechaCrea = DateTime.Now;
+
+            //LLENAR DATA DE AUDITORIA
+            tbTipoDeduccion.tde_UsuarioCrea = 1;
+            tbTipoDeduccion.tde_FechaCrea = DateTime.Now;
+
+            //VARIABLE DONDE SE ALMACENARA EL RESULTADO DEL PROCESO
+            string response = String.Empty;
+            IEnumerable<object> listTipoDeduccion = null;
+            string MensajeError = "";
+            //VALIDAR SI EL MODELO ES VÁLIDO
+            if (ModelState.IsValid)
+            {
+                 try {
+                    
+                    listTipoDeduccion = db.UDP_Plani_tbTipoDeduccion_Update(tbTipoDeduccion.tde_IdTipoDedu,
+                                                                            tbTipoDeduccion.tde_Descripcion, 
+                                                                            1,
+                                                                            DateTime.Now);
+
+                 foreach (UDP_Plani_tbTipoDeduccion_Update_Result resultado in listTipoDeduccion)
+                 MensajeError = resultado.MensajeError;
+
+              if(MensajeError.StartsWith("-1"))
+                {
+                        ModelState.AddModelError("", "No se pudo modificar el registro, contacte al administrador");
+                        response = "error";
+                }
+            }
+              catch(Exception Ex)
+            {
+                    //EN CASO DE CAER EN EL CATCH, IGUALAMOS LA VARIABLE "RESPONSE" A ERROR PARA VALIDARLO EN EL CLIENTE
+                    ModelState.AddModelError("", "No se pudo modificar el registro, contacte al administrador.");
+                    response = "error";
+                }
+                //RETORNAR MENSAJE DE CONFIRMACIÓN EN CASO QUE NO HAYA CAIDO EN EL CATCH
+                response = "bien";
+          }
+            else
+            {
+                // SI EL MODELO NO ES CORRECTO, RETORNAR ERROR
+                ModelState.AddModelError("", "No se pudo modificar el registro, contacte al administrador");
+                response = "error";
+            }
+            ViewBag.tde_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTipoDeduccion.tde_UsuarioCrea);
+            ViewBag.tde_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTipoDeduccion.tde_UsuarioModifica);
+            // RETORNAR MENSAJE AL LADO DEL CLIENTE
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Inactivar(int? ID)
+        {
+            string response = String.Empty;
+            IEnumerable<object> listTipoDeduccion = null;
+            string MensajeError = "";
+            try
+            {
+
+                listTipoDeduccion = db.UDP_Plani_tbTipoDeduccion_Inactivar(ID, 1, DateTime.Now);
+
+                foreach (UDP_Plani_tbTipoDeduccion_Inactivar_Result resultado in listTipoDeduccion.ToList())
+                    MensajeError = resultado.MensajeError;
+
+                if (MensajeError.StartsWith("-1"))
+                {
+                    ModelState.AddModelError("", "No se pudo modificar el registro, contacte al administrador");
+                    response = "error";
+                }
+            }
+            catch (Exception Ex)
+            {
+                //EN CASO DE CAER EN EL CATCH, IGUALAMOS LA VARIABLE "RESPONSE" A ERROR PARA VALIDARLO EN EL CLIENTE
+                ModelState.AddModelError("", "No se pudo modificar el registro, contacte al administrador.");
+                response = "error";
+            }
+            db.Configuration.ProxyCreationEnabled = false;
+            tbTipoDeduccion tbTipoDeduccionesJSON = db.tbTipoDeduccion.Find(ID);
+            return Json(tbTipoDeduccionesJSON, JsonRequestBehavior.AllowGet);
+        } 
+
+        //public ActionResult Inactivo(int? id)
+        //{
+        //    string response = String.Empty;
+        //    IEnumerable<object> listTipoDeduccion = null;
+        //    string MensajeError = "";
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            listTipoDeduccion = db.UDP_Plani_tbTipoDeduccion_Inactivar(id, 1, DateTime.Now);
+
+        //            foreach (UDP_Plani_tbTipoDeduccion_Inactivar_Result resultado in listTipoDeduccion)
+        //                MensajeError = resultado.MensajeError;
+
+        //            if(MensajeError.StartsWith("-1"))
+        //            {
+        //                //EN CASO DE OCURRIR UN ERROR, IGUALAMOS LA VARIABLE "RESPONSE" A ERROR PARA VALIDARLO EN EL CLIENTE
+        //                ModelState.AddModelError("", "No se pudo ingresar el registro, contacte al administrador");
+        //                response = "error";
+        //            }
+        //        }catch(Exception Ex)
+        //        {
+        //            ModelState.AddModelError("", "No se pudo ingresar el registro, contacte al administrador");
+        //            response = "error";
+        //        }
+        //        response = "bien";
+        //      }
+        //    else
+        //    {
+        //        response = "error";
+        //    }
+        //    return Json(response, JsonRequestBehavior.AllowGet);
+        //}
+
+        // GET: TipoDeducciones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbTipoDeduccion tbTipoDeduccion = db.tbTipoDeduccion.Find(id);
+            if (tbTipoDeduccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbTipoDeduccion);
+        }
+
+        // POST: TipoDeducciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbTipoDeduccion tbTipoDeduccion = db.tbTipoDeduccion.Find(id);
+            db.tbTipoDeduccion.Remove(tbTipoDeduccion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ERP_GMEDINA/Scripts/app/General/TipoDeducciones.js
@@ -0,0 +1,202 @@
+﻿//VARIABLE GLOBAL PARA INACTIVAR
+
+var inactivar = 0;
+
+//FUNCION GENERICA PARA REUTILIZAR AJAX
+function _ajax(params, uri, type, callback) {
+    $.ajax({
+        url: uri,
+        type: type,
+        data: { params },
+        success: function (data) {
+            callback(data);
+        }
+    });
+}
+
+//FUNCION: CARGAR DATA Y REFRESCAR LA TABLA DEL INDEX
+function cargarGridTipoDeducciones() {
+    _ajax(null,
+        '/TipoDeducciones/GetData',
+        'GET',
+        (data) => {
+            if (data.length == 0) {
+                //Validar si se genera un error al cargar de nuevo el grid
+                iziToast.error({
+                    title: 'Error',
+                    message: 'No se pudo cargar la información, contacte al administrador',
+                });
+            }
+            //GUARDAR EN UNA VARIABLE LA DATA OBTENIDA
+            var ListaTipoDeducciones = data, template = '';
+            //RECORRER DATA OBETINA Y CREAR UN "TEMPLATE" PARA REFRESCAR EL TBODY DE LA TABLA DEL INDEX
+            for (var i = 0; i < ListaTipoDeducciones.length; i++) {
+                template += '<tr data-id = "' + ListaTipoDeducciones[i].tde_IdTipoDedu + '">' +
+                    '<td>' + ListaTipoDeducciones[i].tde_Descripcion + '</td>' +
+                    '<td>' +
+                    '<button type="button" class="btn btn-default btn-xs" id="btnEditarTipoDeducciones">Editar</button>'+
+                    '<button type="button" class="btn btn-primary btn-xs" id="btnDetalleTipoDeduccion">Detalle</button>' +
+                    '</td>' +
+                    '</tr>';
+            }
+            //REFRESCAR EL TBODY DE LA TABLA DEL INDEX
+            $('#tbodyTipoDeducciones').html(template);
+        });
+}
+
+//FUNCION: PRIMERA FASE DE AGREGAR UN NUEVO REGISTRO, MOSTRAR MODAL DE CREATE
+$(document).on("click", "#btnAgregarTipoDeducciones", function () {
+    //PEDIR DATA PARA LLENAR EL DROPDOWNLIST DEL MODAL
+    //$.ajax({
+    //    url: "/TipoDeducciones/EditGetDDL",
+    //    method: "GET",
+    //    dataType: "json",
+    //    contentType: "application/json; charset=utf-8"
+    //})
+    //LLENAR EL DROPDONWLIST DEL MODAL CON LA DATA OBTENIDA
+    //.done(function (data) {
+    //    $("#Crear #tde_IdTipoDedu").empty();
+    //    $("#Crear #tde_IdTipoDedu").append("<option value='0'>Selecione una opción...</option>");
+    //    $.each(data, function (i, iter) {
+    //        $("#Crear #tde_IdTipoDedu").append("<option value='" + iter.Id + "'>" + iter.Descripcion + "</option>");
+    //    });
+    //});
+
+    //MOSTRAR EL MODAL DE AGREGAR
+    $("#AgregarTipoDeducciones").modal();
+});
+
+//FUNCION: CREAR EL NUEVO REGISTRO
+$('#btnCreateRegistroTipoDeducciones').click(function(){
+    // SIEMPRE HACER LAS RESPECTIVAS VALIDACIONES DEL LADO DEL CLIENTE
+    
+    //SERIALIZAR EL FORMULARIO DEL MODAL (ESTÁ EN LA VISTA PARCIAL)
+    var data = $("#frmTipoDeduccionCreate").serializeArray();
+    //ENVIAR DATA AL SERVIDOR PARA EJECUTAR LA INSERCIÓN
+    $.ajax({
+        url: "/TipoDeducciones/Create",
+        method: "POST",
+        data: data
+    }).done(function (data) {
+        //CERRAR EL MODAL DE AGREGAR
+        $("#AgregarTipoDeducciones").modal('hide');
+        //VALIDAR RESPUESTA OBETNIDA DEL SERVIDOR, SI LA INSERCIÓN FUE EXITOSA O HUBO ALGÚN ERROR
+        if (data == "error") {
+            iziToast.error({
+                title: 'Error',
+                message: 'No se pudo guardar el registro, contacte al administrador',
+            });
+        }
+        else {
+            cargarGridTipoDeducciones();
+            // Mensaje de exito cuando un registro se ha guardado bien
+            iziToast.success({
+                title: 'Exito',
+                message: 'El registro fue registrado de forma exitosa!',
+            });
+        }
+    });
+});
+
+//FUNCION: PRIMERA FASE DE EDICION DE REGISTROS, MOSTRAR MODAL CON LA INFORMACIÓN DEL REGISTRO SELECCIONADO
+$(document).on("click", "#tblTipoDeducciones tbody tr td #btnEditarTipoDeducciones", function () {
+    
+    var ID = $(this).closest('tr').data('id');
+    inactivar = ID;
+    console.log(ID);
+    $.ajax({
+        url: "/TipoDeducciones/Edit/" + ID,
+        method: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({ ID: ID })
+    })
+        .done(function (data) {
+            //SI SE OBTIENE DATA, LLENAR LOS CAMPOS DEL MODAL CON ELLA
+            if (data) {
+                console.log(data);
+                //debugger;
+                $.each(data, function (i, iter) {
+                    $("#Editar #tde_IdTipoDedu").val(iter.tde_IdTipoDedu);
+                    $("#Editar #tde_Descripcion").val(iter.tde_Descripcion);
+                });
+                $("#EditarTipoDeducciones").modal();
+                cargarGridTipoDeducciones();
+            }
+            else {
+                //Mensaje de error si no hay data
+                iziToast.error({
+                    title: 'Error',
+                    message: 'No se pudo cargar la información, contacte al administrador',
+                });
+            }
+        });
+});
+
+//EJECUTAR EDICIÓN DEL REGISTRO EN EL MODAL
+$("#btnUpdateTipoDeducciones").click(function () {
+    //SERIALIZAR EL FORMULARIO (QUE ESTÁ EN LA VISTA PARCIAL) DEL MODAL, SE PARSEA A FORMATO JSON
+    var data = $("#frmTipoDeduccionEdit").serializeArray();
+    //SE ENVIA EL JSON AL SERVIDOR PARA EJECUTAR LA EDICIÓN
+    $.ajax({
+        url: "/TipoDeducciones/Edit",
+        method: "POST",
+        data: data
+    }).done(function (data) {
+        if (data == "error") {
+            //Cuando traiga un error del backend al guardar la edicion
+            iziToast.error({
+                title: 'Error',
+                message: 'No se pudo editar el registro, contacte al administrador',
+            });
+        }
+        else {
+            // REFRESCAR UNICAMENTE LA TABLA
+            cargarGridTipoDeducciones();
+            //UNA VEZ REFRESCADA LA TABLA, SE OCULTA EL MODAL
+            $("#EditarTipoDeducciones").modal('hide');
+            //Mensaje de exito de la edicion
+            iziToast.success({
+                title: 'Exito',
+                message: 'El registro fue editado de forma exitosa!',
+            });
+        }
+    });
+});
+
+//FUNCION: PRIMERA FASE DE EDICION DE REGISTROS, MOSTRAR MODAL CON LA INFORMACIÓN DEL REGISTRO SELECCIONADO
+$("#btnInactivarTipoDeducciones").click(function () {
+    console.log(inactivar);
+    $.ajax({
+        url: "/TipoDeducciones/Inactivar/" + inactivar,
+        method: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({ ID: inactivar })
+    })
+        .done(function (data) {
+            //SI SE OBTIENE DATA, LLENAR LOS CAMPOS DEL MODAL CON ELLA
+            if (data) {
+                console.log(data);
+                //debugger;
+                $.each(data, function (i, iter) {
+                    $("#Editar #tde_IdTipoDedu").val(iter.tde_IdTipoDedu);                
+                    $("#Editar #tde_UsuarioModifica").val(iter.tde_UsuarioModifica);
+                    $("#Editar #tde_FechaModifica").val(iter.tde_FechaModifica);
+                });
+                $("#EditarTipoDeducciones").modal();
+            }
+            else {
+                //Mensaje de error si no hay data
+                iziToast.error({
+                    title: 'Error',
+                    message: 'No se pudo cargar la información, contacte al administrador',
+                });
+            }
+        });
+});
+
+//FUNCION: OCULTAR MODAL DE EDICIÓN
+$("#btnCerrarEditarTipoDeducciones").click(function () {
+    $("#EditarTipoDeducciones").modal('hide');
+});
