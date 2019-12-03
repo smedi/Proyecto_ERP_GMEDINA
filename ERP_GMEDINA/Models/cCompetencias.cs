6726f7e284579f6936c1e660e08451d62d404961
AxelGuzman <53068443+AxelGuzman@users.noreply.github.com>
2019-12-03 10:36:00 -0600

    validado
    final

--- ERP_GMEDINA/Models/cCompetencias.cs
+++ ERP_GMEDINA/Models/cCompetencias.cs
@@ -15,18 +15,28 @@ namespace ERP_GMEDINA.Models
     {
         [Display(Name = "ID")]
         public int comp_Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo \"{0}\"es requerido")]
+        [MaxLength(100, ErrorMessage = "Excedió el número máximo de carácteres")]
         [Display(Name = "Descripcion")]
         public string comp_Descripcion { get; set; }
+
         [Display(Name = "Estado")]
         public bool comp_Estado { get; set; }
+        
+        [MaxLength(100, ErrorMessage = "Excedió el número máximo de carácteres")]
         [Display(Name = "Razon Inactivo")]
         public string comp_RazonInactivo { get; set; }
+
         [Display(Name = "Usuario Crea")]
         public int comp_UsuarioCrea { get; set; }
+
         [Display(Name = "Fecha Crea")]
         public System.DateTime comp_FechaCrea { get; set; }
+
         [Display(Name = "Usuario Modifica")]
         public Nullable<int> comp_UsuarioModifica { get; set; }
+
         [Display(Name = "Fecha Modifica")]
         public Nullable<System.DateTime> comp_FechaModifica { get; set; }
 
