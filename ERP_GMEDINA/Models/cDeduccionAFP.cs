fa89a8201f047b9651ba8e58fab85bb419943412
MalcomMedina <malcom_medina@outlook.com>
2019-12-05 12:35:08 -0600

    Pantallas probadas y Vista Parcial de DeduccionAFP creada

---
+++ ERP_GMEDINA/Models/cDeduccionAFP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ERP_GMEDINA.Models
+{
+    [MetadataType(typeof(cDeduccionAFP))]
+    public partial class tbDeduccionAFP
+    {
+    }
+
+    public class cDeduccionAFP
+    {
+
+    }
+
+}
