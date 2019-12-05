998346e648a7f58fc963d32fb53eb1116f60abce
Murcia7144 <57416349+Murcia7144@users.noreply.github.com>
2019-12-05 14:37:29 -0600

    Vistas Parciales para Validaciones

--- ERP_GMEDINA/Models/cDeduccionAFP.cs
+++ ERP_GMEDINA/Models/cDeduccionAFP.cs
@@ -13,7 +13,39 @@ namespace ERP_GMEDINA.Models
 
     public class cDeduccionAFP
     {
+		[Display(Name = "Id Deducción AFP")]
+		public int dafp_Id { get; set; }
 
-    }
+		[Range(0.01, 9999999999999999.99, ErrorMessage = "El Aporte no puede ser menor de 0 dígitos, ni mayor que 10 dígitos")]
+		[Required(ErrorMessage = "Campo Requerido")]
+		[Display(Name = "Aporte en Lps")]
+		public decimal dafp_AporteLps { get; set; }
+
+		[Required(ErrorMessage = "Campo Requerido")]
+		[Display(Name = "AFP")]
+		public int afp_Id { get; set; }
+
+		[Required(ErrorMessage = "Campo Requerido")]
+		[Display(Name = "Empleado")]
+		public int emp_Id { get; set; }
+
+		[Display(Name = "Pagado")]
+		public Nullable<bool> dafp_Pagado { get; set; }
+
+		[Display(Name = "Creado por")]
+		public int dafp_UsuarioCrea { get; set; }
+
+		[Display(Name = "Fecha de Creación")]
+		public System.DateTime dafp_FechaCrea { get; set; }
+
+		[Display(Name = "Modificado por")]
+		public Nullable<int> dafp_UsuarioModifica { get; set; }
+
+		[Display(Name = "Fecha de Modificación")]
+		public Nullable<System.DateTime> dafp_FechaModifica { get; set; }
+
+		[Display(Name = "Activo")]
+		public bool dafp_Activo { get; set; }
+	}
 
 }
