4239f9ab9dbb5297d3edfc68a6483567eae0379c
carol2198 <53068732+carol2198@users.noreply.github.com>
2019-12-06 11:28:23 -0600

    Carol
    Lupe

--- ERP_GMEDINA/Controllers/SeleccionCandidatosController.cs
+++ ERP_GMEDINA/Controllers/SeleccionCandidatosController.cs
@@ -51,6 +51,7 @@ namespace ERP_GMEDINA.Controllers
                 return Json("-2", JsonRequestBehavior.AllowGet);
             }
         }
+
         public ActionResult ChildRowData(int? id)
         {
             //declaramos la variable de coneccion solo para recuperar los datos necesarios.
@@ -70,18 +71,18 @@ namespace ERP_GMEDINA.Controllers
         }
         public ActionResult llenarDropDowlist()
         {
-            var Sucursales = new List<object> { };
+            var Identidad = new List<object> { };
             using (db = new ERP_GMEDINAEntities())
             {
                 try
                 {
-                    Sucursales.Add(new
+                    Identidad.Add(new
                     {
                         Id = 0,
                         Descripcion = "**Seleccione una opción**"
                     });
-                    Sucursales.AddRange(db.tbSucursales
-                    .Select(tabla => new { Id = tabla.suc_Id, Descripcion = tabla.suc_Descripcion })
+                    Identidad.AddRange(db.tbPersonas
+                    .Select(tabla => new { Id = tabla.per_Id, Descripcion = tabla.per_Identidad })
                     .ToList());
                 }
                 catch
@@ -93,7 +94,7 @@ namespace ERP_GMEDINA.Controllers
 
             }
             var result = new Dictionary<string, object>();
-            result.Add("Sucursales", Sucursales);
+            result.Add("Identidad", Identidad);
             return Json(result, JsonRequestBehavior.AllowGet);
         }
         // GET: Areas/Details/5
@@ -105,12 +106,12 @@ namespace ERP_GMEDINA.Controllers
             }
             //declaramos la variable de coneccion solo para recuperar los datos necesarios.
             //posteriormente es destruida.
-            tbAreas tbAreas = null;
+            tbSeleccionCandidatos tbSeleccionCandidatos = null;
             using (db = new ERP_GMEDINAEntities())
             {
                 try
                 {
-                    tbAreas = db.tbAreas.Find(id);
+                    tbSeleccionCandidatos = db.tbSeleccionCandidatos.Find(id);
                 }
                 catch
                 {
@@ -119,157 +120,157 @@ namespace ERP_GMEDINA.Controllers
 
                 }
             }
-            if (tbAreas == null)
+            if (tbSeleccionCandidatos == null)
             {
                 return HttpNotFound();
             }
-            return View(tbAreas);
+            return View(tbSeleccionCandidatos);
         }
         // GET: Areas/Create
-        public ActionResult Create()
-        {
-            //declaramos la variable de coneccion solo para recuperar los datos necesarios.
-            //posteriormente es destruida.
-            List<tbSucursales> Sucursales = new List<tbSucursales> { };
-            ViewBag.suc_Id = new SelectList(Sucursales, "suc_Id", "suc_Descripcion");
-            return View();
-        }
-        // POST: Areas/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        public ActionResult Create(tbAreas tbAreas, tbDepartamentos[] tbDepartamentos)
-        {
-            //declaramos la variable de coneccion solo para recuperar los datos necesarios.
-            //posteriormente es destruida.
-            string result = "";
-            //en esta area ingresamos el registro con el procedimiento almacenado
-            try
-            {
-                if (tbAreas.suc_Id == 0 || tbAreas.tbCargos.car_Descripcion == "" || tbAreas.area_Descripcion == "")
-                {
-                    return Json("-2", JsonRequestBehavior.AllowGet);
-                }
-                foreach (var item in tbDepartamentos)
-                {
-                    if (item.depto_Descripcion == "" || item.tbCargos.car_Descripcion == "")
-                    {
-                        return Json("-2", JsonRequestBehavior.AllowGet);
-                    }
-                }
-                var Usuario = (tbUsuario)Session["Usuario"];
-                using (var scope = new TransactionScope())
-                {
-                    using (db = new ERP_GMEDINAEntities())
-                    {
-                        var list = db.UDP_RRHH_tbAreas_Insert(
-                                                                tbAreas.suc_Id,
-                                                                tbAreas.tbCargos.car_Descripcion,
-                                                                tbAreas.area_Descripcion,
-                                                                Usuario.usu_Id,
-                                                                DateTime.Now);
-                        foreach (UDP_RRHH_tbAreas_Insert_Result item in list)
-                        {
-                            tbAreas.area_Id = int.Parse(item.MensajeError.ToString());
-                        }
-                        if (tbAreas.area_Id == -2)
-                        {
-                            return Json("-2", JsonRequestBehavior.AllowGet);
-                        }
-                        foreach (var item in tbDepartamentos)
-                        {
+//        public ActionResult Create()
+//        {
+//            //declaramos la variable de coneccion solo para recuperar los datos necesarios.
+//            //posteriormente es destruida.
+//            List<tbSucursales> Sucursales = new List<tbSucursales> { };
+//            ViewBag.suc_Id = new SelectList(Sucursales, "suc_Id", "suc_Descripcion");
+//            return View();
+//        }
+//        // POST: Areas/Create
+//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        public ActionResult Create(tbAreas tbAreas, tbDepartamentos[] tbDepartamentos)
+//        {
+//            //declaramos la variable de coneccion solo para recuperar los datos necesarios.
+//            //posteriormente es destruida.
+//            string result = "";
+//            //en esta area ingresamos el registro con el procedimiento almacenado
+//            try
+//            {
+//                if (tbAreas.suc_Id == 0 || tbAreas.tbCargos.car_Descripcion == "" || tbAreas.area_Descripcion == "")
+//                {
+//                    return Json("-2", JsonRequestBehavior.AllowGet);
+//                }
+//                foreach (var item in tbDepartamentos)
+//                {
+//                    if (item.depto_Descripcion == "" || item.tbCargos.car_Descripcion == "")
+//                    {
+//                        return Json("-2", JsonRequestBehavior.AllowGet);
+//                    }
+//                }
+//                var Usuario = (tbUsuario)Session["Usuario"];
+//                using (var scope = new TransactionScope())
+//                {
+//                    using (db = new ERP_GMEDINAEntities())
+//                    {
+//                        var list = db.UDP_RRHH_tbAreas_Insert(
+//                                                                tbAreas.suc_Id,
+//                                                                tbAreas.tbCargos.car_Descripcion,
+//                                                                tbAreas.area_Descripcion,
+//                                                                Usuario.usu_Id,
+//                                                                DateTime.Now);
+//                        foreach (UDP_RRHH_tbAreas_Insert_Result item in list)
+//                        {
+//                            tbAreas.area_Id = int.Parse(item.MensajeError.ToString());
+//                        }
+//                        if (tbAreas.area_Id == -2)
+//                        {
+//                            return Json("-2", JsonRequestBehavior.AllowGet);
+//                        }
+//                        foreach (var item in tbDepartamentos)
+//                        {
 
-                        }
+//                        }
 
 
 
-                    }
-                }
+//                    }
+//                }
 
-            }
-            catch (Exception ex)
-            {
-                ex.Message.ToString();
-                result = "-2";
-            }
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-        // GET: Areas/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            //declaramos la variable de coneccion solo para recuperar los datos necesarios.
-            //posteriormente es destruida.
-            tbAreas tbAreas = null;
-            using (db = new ERP_GMEDINAEntities())
-            {
-                List<tbSucursales> Sucursales = null;
-                try
-                {
-                    tbAreas = db.tbAreas.Find(id);
-                    Sucursales = db.tbSucursales.ToList();
-                    ViewBag.suc_Id = new SelectList(Sucursales, "suc_Id", "suc_Descripcion");
-                }
-                catch
-                {
-                }
-            }
-            if (tbAreas == null)
-            {
-                return HttpNotFound();
-            }
-            return View(new cAreas
-            {
-                suc_Id = tbAreas.suc_Id,
-                area_Descripcion = tbAreas.area_Descripcion
-            });
-        }
-        // POST: Areas/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        public ActionResult Edit([Bind(Include = "area_Id,car_Id,suc_Id,area_Descripcion,area_Estado,area_Razoninactivo,area_Usuariocrea,area_Fechacrea,area_Usuariomodifica,area_Fechamodifica")] tbAreas tbAreas)
-        {
-            //declaramos la variable de coneccion solo para recuperar los datos necesarios.
-            //posteriormente es destruida.
-            string result = "";
-            using (db = new ERP_GMEDINAEntities())
-            {
-                try
-                {
-                    //en esta area actualizamos el registro con el procedimiento almacenado
-                }
-                catch
-                {
-                    result = "-2";
-                }
-            }
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-        // POST: Areas/Delete/5
+//            }
+//            catch (Exception ex)
+//            {
+//                ex.Message.ToString();
+//                result = "-2";
+//            }
+//            return Json(result, JsonRequestBehavior.AllowGet);
+//        }
+//        // GET: Areas/Edit/5
+//        public ActionResult Edit(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            //declaramos la variable de coneccion solo para recuperar los datos necesarios.
+//            //posteriormente es destruida.
+//            tbAreas tbAreas = null;
+//            using (db = new ERP_GMEDINAEntities())
+//            {
+//                List<tbSucursales> Sucursales = null;
+//                try
+//                {
+//                    tbAreas = db.tbAreas.Find(id);
+//                    Sucursales = db.tbSucursales.ToList();
+//                    ViewBag.suc_Id = new SelectList(Sucursales, "suc_Id", "suc_Descripcion");
+//                }
+//                catch
+//                {
+//                }
+//            }
+//            if (tbAreas == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(new cAreas
+//            {
+//                suc_Id = tbAreas.suc_Id,
+//                area_Descripcion = tbAreas.area_Descripcion
+//            });
+//        }
+//        // POST: Areas/Edit/5
+//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        public ActionResult Edit([Bind(Include = "area_Id,car_Id,suc_Id,area_Descripcion,area_Estado,area_Razoninactivo,area_Usuariocrea,area_Fechacrea,area_Usuariomodifica,area_Fechamodifica")] tbAreas tbAreas)
+//        {
+//            //declaramos la variable de coneccion solo para recuperar los datos necesarios.
+//            //posteriormente es destruida.
+//            string result = "";
+//            using (db = new ERP_GMEDINAEntities())
+//            {
+//                try
+//                {
+//                    //en esta area actualizamos el registro con el procedimiento almacenado
+//                }
+//                catch
+//                {
+//                    result = "-2";
+//                }
+//            }
+//            return Json(result, JsonRequestBehavior.AllowGet);
+//        }
+//        // POST: Areas/Delete/5
 
-[HttpPost]
-        public ActionResult Delete(int? id)
-        {
-            //declaramos la variable de coneccion solo para recuperar los datos necesarios.
-            //posteriormente es destruida.
-            string result = "";
-            using (db = new ERP_GMEDINAEntities())
-            {
-                try
-                {
-                    //en esta area Inavilitamos el registro con el procedimiento almacenado
-                }
-                catch
-                {
-                    result = "-2";
-                }
-            }
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
+//[HttpPost]
+//        public ActionResult Delete(int? id)
+//        {
+//            //declaramos la variable de coneccion solo para recuperar los datos necesarios.
+//            //posteriormente es destruida.
+//            string result = "";
+//            using (db = new ERP_GMEDINAEntities())
+//            {
+//                try
+//                {
+//                    //en esta area Inavilitamos el registro con el procedimiento almacenado
+//                }
+//                catch
+//                {
+//                    result = "-2";
+//                }
+//            }
+//            return Json(result, JsonRequestBehavior.AllowGet);
+//        }
 
 
         protected override void Dispose(bool disposing)
