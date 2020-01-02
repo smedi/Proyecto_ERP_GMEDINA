650bf332d6e1b176759b0c7672ff99a478455c06
LuisR-FF <57548868+LuisR-FF@users.noreply.github.com>
2020-01-02 12:48:13 -0600

    loading

--- ERP_GMEDINA/Scripts/app/General/HistorialIncapacidades/IndexHistorialIncapacidades.js
+++ ERP_GMEDINA/Scripts/app/General/HistorialIncapacidades/IndexHistorialIncapacidades.js
@@ -82,6 +82,7 @@ $('#IndexTable tbody').on('click', 'td.details-control', function () {
             function (obj) {
                 if (obj != "-1" && obj != "-2" && obj != "-3") {
                     row.child(format(obj)).show();
+                    tr.removeClass('loading');
                     tr.addClass('shown');
                 }
             });
