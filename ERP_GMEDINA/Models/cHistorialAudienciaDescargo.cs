8508baddfd201ae0df7ddffed63546d1afdb7fb0
AllanBennett27 <53162492+AllanBennett27@users.noreply.github.com>
2020-01-09 21:50:28 -0600

    Validaciones

--- ERP_GMEDINA/Models/cHistorialAudienciaDescargo.cs
+++ ERP_GMEDINA/Models/cHistorialAudienciaDescargo.cs
@@ -13,13 +13,18 @@ namespace ERP_GMEDINA.Models
     }
     public class cHistorialAudienciaDescargo
     {
+
        [Display(Name ="Motivo")]
+       [Required(AllowEmptyStrings =false, ErrorMessage ="El campo \"{0}\" es requerido")]
+       [MaxLength(100,ErrorMessage ="Excedio el número maximo de caracteres")]
         public string aude_Descripcion { get; set; }
         [Display(Name = "Fecha Audiencia")]
         public System.DateTime aude_FechaAudiencia { get; set; }
         [Display(Name = "Testigo")]
         public bool aude_Testigo { get; set; }
         [Display(Name = "Direccion Archivo")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo \"{0}\" es requerido")]
+        [MaxLength(100, ErrorMessage = "Excedio el número maximo de caracteres")]
         public string aude_DireccionArchivo { get; set; }
         [Display(Name = "Estado")]
         public bool aude_Estado { get; set; }
